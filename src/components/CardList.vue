9b44fb4156ffcb73b59325ed8064f1446246dc6a
Abylay <ayazbekabylai@gmail.com>
2025-04-15 22:54:21 +0500

    add Card and CardList component

---
+++ src/components/CardList.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Card from './Card.vue'
+const onClickAdd = () => {
+  alert('Добавить!')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="59990"
+      :isFavorite="false"
+      :onClickAdd="onClickAdd"
+    />
+  </div>
+</template>
